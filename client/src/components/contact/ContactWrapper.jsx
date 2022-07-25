c2045c41a3f445f28907baf17d14d42e947cad83
Exequiel65 <marcos97britos@gmail.com>
2022-07-25 16:05:38 -0300

    finished page contact

--- client/src/components/contact/ContactWrapper.jsx
+++ client/src/components/contact/ContactWrapper.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
-const ContactWrapper = () => {
+const ContactWrapper = ({contact}) => {
+    console.log(contact)
     return (
         <section className='flex-colummn section-contact-contain'>
             <div className='iframe-map-goole-container'>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.8945457995665!2d-64.1867856842483!3d-31.41703140342805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a283431cda1b%3A0x14657a81f201cf9a!2sC4054!5e0!3m2!1ses!2sar!4v1658766075044!5m2!1ses!2sar" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src={contact.iframeMaps} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div className='contact-container'>
                 <ul>
-                    <li><i className="fas fa-map-marker-alt"></i><a href='https://goo.gl/maps/3Bep3YA1F3LiMQmu6' target='_blank'>X5022 Córdoba</a></li>
+                    <li><i className="fas fa-map-marker-alt"></i><a href={contact.addressLinkMaps[0]} target='_blank'>{contact.address[0]}</a></li>
                     <li><i className="far fa-clock"></i>
                         Horarios:
                         <ul className='hours-job'>
-                            <li>Lunes a Viernes : 08:00hs a 18:00hs</li>
-                            <li>Sabados : 08:00hs a 13:00hs</li>
+                            {contact.hours.map((d, i)=>(
+                                <li>{`${d.days} : ${d.hour}`}</li>
+                            ))}
+                            
                         </ul>
                     </li>
-                    <li><i className="fas fa-phone"></i>+543513667788</li>
-                    <li><i className="far fa-envelope"></i><a href='mailto:e-commercer@gmail.com'> e-commercer@gmail.com</a></li>
+                    <li><i className="fas fa-phone"></i><a href={`tel:${contact.phone[0]}`}>+{contact.phone[0]}</a></li>
+                    <li><i className="far fa-envelope"></i><a href={`mailto:${contact.email[0]}`}>{contact.email[0]}</a></li>
                 </ul>
             </div>
         </section>
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import ContactWrapper from '../components/contact/ContactWrapper';
 import FirstVist from '../components/firstVist';
+import { FetchGet } from '../helpers/AxiosMethod';
 import '../styles/contact.css'
 const Contact = () => {
+    const [Contact, setContact] = useState(null);
+    const apiGet = async ()=>{
+        let contact = await FetchGet('/contact')
+        setContact(contact.data.contact[0])
+    }
+    useEffect(() => {
+        apiGet()
+    }, []);
     return (
         <main className='main-contact'>
             <FirstVist title='Contact' descr='Lorem, ipsum dolor sit amet' />
-            <ContactWrapper />
+            {Contact && <ContactWrapper contact={Contact} />}
+            
         </main>
     );
 }
