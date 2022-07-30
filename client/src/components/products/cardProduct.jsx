c907dcf026044916ef0158ed560caba8a5e78d36
Exequiel65 <marcos97britos@gmail.com>
2022-07-29 21:43:18 -0300

    add filter layout Products

--- client/src/components/products/cardProduct.jsx
+++ client/src/components/products/cardProduct.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { toThousand } from "../../helpers/toThousand";
 
-const CardProduct = () => {
+const CardProduct = ({product}) => {
   return (
     <Link to="/" className="card-product">
       <div className="image-container">
-        <img src="/img/founder1.png" alt="" />
+        <img src={product.images[0]} alt="" />
       </div>
       <div className="data-products">
-        <p>Name</p>
-        <p>$ 50000</p>
+        <p>{product.name}</p>
+        <p>$ {toThousand(product.price)}</p>
       </div>
     </Link>
   );
